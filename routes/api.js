696464e04d44cf12f95cfafc882bc64c4531ade1
Burchell <jlkaline@me.com>
2021-05-01 22:36:51 -0400

    api findone

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
         .then(dbWorkout => {
@@ -12,15 +13,44 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 router.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.json(err);
-    });
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
+router.get("/api/workouts/:id", (req, res) => {
 
+    const { id } = req.params;
+    Workout.findOne({
+        _id: id,
+    }).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+        .catch(err => {
+            res.status(400).json
+        })
 
-module.exports = router;
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+
+    const id = params.id;
+    let savedExercises = [];
+
+    db.Workout.find({ _id: id })
+        .then(dbWorkout => {
+            savedExercises = dbWorkout[0].exercises;
+            res.json(savedExercises);
+            let allExercises = [...savedExercises, body];
+            updateWorkout(allExercises);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+
+    });
+    module.exports = router;
